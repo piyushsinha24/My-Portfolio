8f0dd207d58cf5cbee452666b99322b261ba6316
piyush <piyush.sinha24@gmail.com>
2019-05-19 17:29:02 +0530

    pageview

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_web/material.dart';
+import './home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Web Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
